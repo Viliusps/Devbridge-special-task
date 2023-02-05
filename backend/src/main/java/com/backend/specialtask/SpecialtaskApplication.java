aa41ce48ee93c27bbdb5a5e05015c685798d4e30
vilpas <viliusps@gmail.com>
2023-02-05 17:11:40 +0200

    Initial setup

---
+++ backend/src/main/java/com/backend/specialtask/SpecialtaskApplication.java
@@ -0,0 +1,13 @@
+package com.backend.specialtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialtaskApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/backend/specialtask/SpecialtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.specialtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecialtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
